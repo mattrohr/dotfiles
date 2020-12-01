d7968737c043d7fba480a82887d22e9b4293d65d
Matt Rohr <67395122+mattrohr@users.noreply.github.com>
2020-12-01 08:14:19 -0600

    link .extras and .prompt scripts to home folder during installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,20 +8,22 @@ sudo -v
 # update existing `sudo` time stamp until finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Install programs references in Brewfile
+# Install programs in Brewfile
 brew bundle
 
-# Configure MacOS settings
+# Configure macOS settings
 ./.macos
 
-# Configure python environment (from: https://opensource.com/article/19/5/python-3-default-mac)
+# Configure Python environment
 ./.python
 
 # Configure shell
 ln -s ${PWD} ~
+ln -s ${PWD}/.prompt ~/.prompt
 ln -s ${PWD}/.aliases ~/.aliases
 ln -s ${PWD}/.exports ~/.exports
 ln -s ${PWD}/.functions ~/.functions
+ln -s ${PWD}/.extra ~/.extra
 ln -s ${PWD}/.gitconfig ~/.gitconfig
 ln -s ${PWD}/.gitignore_global ~/.gitignore_global
 ln -s ${PWD}/.hushlogin ~/.hushlogin
